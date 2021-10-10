e81d978840568aec34295c2568fcc007994b5270
NavinShrinivas <karupal2002@gmail.com>
2021-10-10 23:08:30 +0530

    adding little redis parts and new map generation

--- src/redirect.py
+++ src/redirect.py
@@ -1,3 +1,7 @@
+import time
+import os
+from dotenv import load_dotenv
+import redis
 import uvicorn
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
@@ -6,9 +10,12 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse
 
+load_dotenv()
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="../templates"), name="static")
 templates = Jinja2Templates(directory="../templates")
+r=redis.Redis(host=os.getenv('REDIS_HOST'),port=os.getenv('REDIS_PORT'))
+
 
 webh=0
 @app.get("/home",include_in_schema=False)
@@ -17,14 +24,26 @@ def index(request: Request):
     webh=webh+1
     return templates.TemplateResponse("redirecthome.html", {"request": request , "wb" : webh})
 
+@app.get("/new",include_in_schema=False)
+def index(request:Request):
+    return templates.TemplateResponse("maplinks.html" , {"request" : request})
+
 # @app.get("/r/{key}")
 # def redirect(key : str):
     # if(key=="g"):
         # return RedirectResponse("https://www.google.com")
 @app.get("/r/{key}", response_class=RedirectResponse, status_code=302)
 async def redirect_short_links(key : str):
-    if(key=="test"):
-        return "https://www.google.com"
+    start=time.time()*1000
+    if(key=="alive"):
+        return "https://www.google.com" #this is test redirect , let it be to check if the service is up and running
+    else:
+        link=r.get(key)
+        print(link)
+        end=time.time()*1000
+        print("Total redirect time : ",end-start)
+        return link.decode('ascii')
+
 
 if __name__ == "__main__":
     tot_webhits = 0
